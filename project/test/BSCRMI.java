b0cc6ce45388926af68bc0846256df3a87d8db1d
Asad Malik <asadsm@utexas.edu>
2017-11-22 22:09:18 -0800

    Added separate test folder which can be run from terminal

---
+++ project/test/BSCRMI.java
@@ -0,0 +1,20 @@
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * This is the interface of Paxos RMI call. You should implement each method defined below.
+ * Prepare is the RMI that proposer sends prepare request to acceptors.
+ * Accept is the RMI that proposer sends accept request to acceptors.
+ * Decide is the RMI that proposer broadcasts decision once consensus reaches.
+ * Please don't change the interface.
+ */
+public interface BSCRMI extends Remote{
+    Response Vote(Request req) throws RemoteException;
+    Response Done(Request req) throws RemoteException;
+    Response Key(Request req) throws RemoteException;
+	Response Choice(Request req) throws RemoteException;
+	Response Ballot(Request req) throws RemoteException;
+    
+
+}
---
+++ project/test/BordaCount.java
@@ -0,0 +1,33 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+//import VoteScore.VoteScore;
+
+public class BordaCount implements BSWScheme {
+
+	@Override
+	public String runAlgoritm(String[] ballot, int faulty) {
+		String result = "";
+		ArrayList<VoteScore> ranking = new ArrayList<VoteScore>();
+		HashMap<String, Integer> scores = new HashMap<>();
+		for (String s: ballot){
+			if (s == null)
+				continue;
+			for (int i = 0; i < s.length(); ++i){
+				Integer occur = scores.get(s.substring(i, i+1));
+				scores.put(s.substring(i, i+1), occur != null ? occur + i : i);
+			}
+		}
+		for (String s: scores.keySet()){
+			ranking.add(new VoteScore(s, scores.get(s)));
+		}
+		Collections.sort(ranking);
+		for (VoteScore vs: ranking){
+			result += vs.vote;
+		}
+		return result;
+	}
+
+}
